c92b9d5f071a5eadb34c1e2ae57e527e08f39604
Aaron44201 <44201@longroad.ac.uk>
2014-10-10 14:17:44 +0100

    ascii task 1

---
+++ ascii.py
@@ -0,0 +1,12 @@
+#Aaron Bentley
+#10/10/14
+#ascii
+
+charecter = int(input("Enter ascii code: "))
+asci = chr(charecter)
+print("{0}".format (asci))
+
+num = input("Enter charecter: ")
+code = ord(num)
+print("{0}".format (code))
+                
