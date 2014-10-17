2d575baf5d2374a5f75e76c7e2c963fc533b5df8
Aaron44201 <44201@longroad.ac.uk>
2014-10-17 14:05:04 +0100

    complete

---
+++ Hello!.md
@@ -0,0 +1,10 @@
+
+
+RE:T2
+
+message <- USERINPUT
+repeat <- USERINPUT
+FOR count in RANGE repeat 
+	OUTPUT message
+
+
---
+++ task2.py
@@ -0,0 +1,8 @@
+#Aaron Bentley
+#task2
+#17/10/14
+
+message = input("enter message ")
+repeat = int(input("times to repeat "))
+for count in range(repeat):
+    print(message)
